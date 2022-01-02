53a25a59112502afb698b668b586e0a14808856b
Safwan Chowdhury <ed18src@leeds.ac.uk>
2022-01-02 14:04:26 +0000

    new app, cleaner ui, laps now flexible

--- src/com/mmr/App.java
+++ src/com/mmr/App.java
@@ -1,8 +1,6 @@
 package com.mmr;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.Arrays;
 
 public class App {
@@ -11,8 +9,7 @@ public class App {
     private JTextField t1Box;
     private JTextField t2Box;
     private JTextField t3Box;
-    private JCheckBox undercutCheckBox;
-    private JComboBox lapsCombo;
+    private JTextField lapsText;
     private JTextField driverT;
     private JComboBox comboBox1;
     public static String compound;
@@ -20,43 +17,23 @@ public class App {
     public static int It1;
     public static int It2;
     public static int It3;
-    public static boolean Undercut;
     public App() {
-        button1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                It1 = Integer.parseInt(t1Box.getText());
-                It2 = Integer.parseInt(t2Box.getText());
-                It3 = Integer.parseInt(t3Box.getText());
-                laps1 = lapsCombo.getSelectedIndex();
-                compound = (String) comboBox1.getSelectedItem();
-                Undercut = undercutCheckBox.isSelected();
-                int[] arr = new int[3];
-                int laps;
-                String sh = compound;
-                arr[0] = It1;
-                arr[1] = It2;
-                arr[2] = It3;
-                String output = new String();
-                if (sh.equals("Soft")) {
-                    if (Undercut) {
-                        output = Arrays.toString(mmr.mmr(arr));
-                    } else {
-                        output = Arrays.toString(mmr.mmru(arr));
-                    }
-                } else if (sh.equals("Hard")) {
-                    laps = laps1;
-                    if (laps == 0) {
-                        output = Arrays.toString(mmr.mmrh8(arr));
-                    } else if (laps == 1) {
-                        output = Arrays.toString(mmr.mmrh10(arr));
-                    } else if (laps == 2) {
-                        output = Arrays.toString(mmr.mmrh12(arr));
-                    }
-                }
-                String stringObject = driverT.getText() + " - " + compound + ": " + output;
-                JOptionPane.showMessageDialog(null, stringObject);
-            }
+        button1.addActionListener(e -> {
+            It1 = Integer.parseInt(t1Box.getText());
+            It2 = Integer.parseInt(t2Box.getText());
+            It3 = Integer.parseInt(t3Box.getText());
+            laps1 = Integer.parseInt(lapsText.getText());
+            compound = (String) comboBox1.getSelectedItem();
+            int[] arr = new int[3];
+            int laps;
+            arr[0] = It1;
+            arr[1] = It2;
+            arr[2] = It3;
+            String output;
+            laps = laps1;
+            output = Arrays.toString(mmr.mmrnew(arr,laps));
+            String stringObject = driverT.getText() + " - " + compound + ": " + output;
+            JOptionPane.showMessageDialog(null, stringObject);
         });
     }
 
--- src/com/mmr/mmr.java
+++ src/com/mmr/mmr.java
@@ -1,194 +1,58 @@
 package com.mmr;
 
 
-import java.util.Arrays;
 
 
 public class mmr {
-    public static String output;
-    public static int[] mmr(int[] array) {
+
+    public static int[] mmrnew(int[] array, int laps){
+        // the length of the input array
+        int base = 3;
         int result = 1000;
-        int[] ideal = new int[0];
         int closestResult = 100;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                for (int k = 0; k < 3; k++) {
-                    for (int m = 0; m < 3; m++) {
-                        result = 100 - array[i] - array[j] - array[k] - array[m];
-                        if (result == 0) {
-                            ideal = new int[]{array[i], array[j], array[k], array[m]};
-                            return ideal;
-                        } else {
-                            if (result < closestResult && result > 0) {
-                                ideal = new int[]{array[i], array[j], array[k], array[m]};
-                                closestResult = result;
-                            }
-                        }
+        // must have same length as desired result length
+        int[] indexArray = new int[laps];
+        int[] tempArray = new int[laps];
+        int[] idealArray = new int[laps];
+        // there are base to the power of length possible combinations
+        for (int counter = 0; counter < Math.pow(base, indexArray.length); counter++) {
+            // retrieve digits as in linked answer
+            int counterCopy = counter;
+            int sum = 0;
+            for (int i = 0; i < indexArray.length; i++) {
+                indexArray[i] = counterCopy % base;
+                counterCopy /= base;
+                if (indexArray[i] == 0){
+                    tempArray[i] = array[2];
+                }
+                else if (indexArray[i] == 1){
+                    tempArray[i] = array[1];
+                }
+                else if (indexArray[i] == 2){
+                    tempArray[i] = array[0];
+                }
+                sum += tempArray[i];
+                result = 100 - sum;
+            }
+            if (result == 0){
+                for (int i = 0; i < indexArray.length; i++){
+                    idealArray[i] = tempArray[i];
+                }
+                return idealArray;
+            }
+            else{
+                if (result < closestResult && result > 0) {
+                    for (int i = 0; i < indexArray.length; i++){
+                        idealArray[i] = tempArray[i];
                     }
+                    closestResult = result;
+                }
+            }
 
-                }
-            }
-        }
-        return ideal;
-    }
-        public static int[] mmru ( int[] array) {
-            int result = 1000;
-            int[] ideal = new int[0];
-            int closestResult = 100;
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    for (int k = 0; k < 3; k++) {
-                            result = 100 - array[i] - array[j] - array[k];
-                            if (result == 0) {
-                                ideal = new int[]{array[i], array[j], array[k]};
-                                return ideal;
-                            } else {
-                                if (result < closestResult && result > 0) {
-                                    ideal = new int[]{array[i], array[j], array[k]};
-                                    closestResult = result;
-                                }
-                            }
 
-                    }
-                }
-            }
-            return ideal;
         }
-    public static int[] mmrh12(int[] array) {
-        int result = 1000;
-        int[] ideal = new int[0];
-        int closestResult = 100;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                for (int k = 0; k < 3; k++) {
-                    for (int l = 0; l < 3; l++) {
-                        for (int m = 0; m < 3; m++) {
-                            for (int n = 0; n < 3; n++) {
-                                for (int o = 0; o < 3; o++) {
-                                    for (int p = 0; p < 3; p++) {
-                                        for (int q = 0; q < 3; q++) {
-                                            for (int r = 0; r < 3; r++) {
-                                                for (int s = 0; s < 3; s++) {
-                                                    for (int t = 0; t < 3; t++) {
-                                                        result = 100 - array[i] - array[j] - array[k] - array[l] - array[m] - array[n] - array[o] - array[p] - array[q] - array[r] - array[s] - array[t];
-                                                        if (result == 0) {
-                                                            ideal = new int[]{array[i], array[j], array[k], array[l], array[m], array[n], array[o], array[p], array[q], array[r], array[s], array[t]};
-                                                            return ideal;
-                                                        } else {
-                                                            if (result < closestResult && result > 0) {
-                                                                ideal = new int[]{array[i], array[j], array[k], array[l], array[m], array[n], array[o], array[p], array[q], array[r], array[s], array[t]};
-                                                                closestResult = result;
-                                                            }
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        return ideal;
-    }
-    public static int[] mmrh10 ( int[] array) {
-        int result = 1000;
-        int[] ideal = new int[0];
-        int closestResult = 100;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                for (int k = 0; k < 3; k++) {
-                    for (int l = 0; l < 3; l++) {
-                        for (int m = 0; m < 3; m++) {
-                            for (int n = 0; n < 3; n++) {
-                                for (int o = 0; o < 3; o++) {
-                                    for (int p = 0; p < 3; p++) {
-                                        for (int q = 0; q < 3; q++) {
-                                            for (int r = 0; r < 3; r++) {
-                                                        result = 100 - array[i] - array[j] - array[k] - array[l] - array[m] - array[n] - array[o] - array[p] - array[q] - array[r];
-                                                        if (result == 0) {
-                                                            ideal = new int[]{array[i], array[j], array[k], array[l], array[m], array[n], array[o], array[p], array[q], array[r]};
-                                                            return ideal;
-                                                        }
-                                                        else {
-                                                            if (result < closestResult && result > 0) {
-                                                                ideal = new int[]{array[i], array[j], array[k], array[l], array[m], array[n], array[o], array[p], array[q], array[r]};
-                                                                closestResult = result;
-                                                            }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        return ideal;
-    }
-    public static int[] mmrh8 ( int[] array) {
-        int result = 1000;
-        int[] ideal = new int[0];
-        int closestResult = 100;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                for (int k = 0; k < 3; k++) {
-                    for (int l = 0; l < 3; l++) {
-                        for (int m = 0; m < 3; m++) {
-                            for (int n = 0; n < 3; n++) {
-                                for (int o = 0; o < 3; o++) {
-                                    for (int p = 0; p < 3; p++) {
-                                                result = 100 - array[i] - array[j] - array[k] - array[l] - array[m] - array[n] - array[o] - array[p];
-                                                if (result == 0) {
-                                                    ideal = new int[]{array[i], array[j], array[k], array[l], array[m], array[n], array[o], array[p]};
-                                                    return ideal;
-                                                }
-                                                else {
-                                                    if (result < closestResult && result > 0) {
-                                                        ideal = new int[]{array[i], array[j], array[k], array[l], array[m], array[n], array[o], array[p]};
-                                                        closestResult = result;
-                                                    }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        return ideal;
+        return idealArray;
     }
 
 
-        public static void main (String[]args) {
-            int[] arr = new int[3];
-            int laps;
-            String sh = App.compound;
-            arr[0] = App.It1;
-            arr[1] = App.It2;
-            arr[2] = App.It3;
-            if (sh.equals("Soft")) {
-                if (App.Undercut) {
-                    output = Arrays.toString(mmr(arr));
-                } else {
-                    output = Arrays.toString(mmru(arr));
-                }
-            } else if (sh.equals("Hard")) {
-                laps = App.laps1;
-                if (laps == 0) {
-                    output = Arrays.toString(mmrh8(arr));
-                } else if (laps == 1) {
-                    output = Arrays.toString(mmrh10(arr));
-                } else if (laps == 2) {
-                    output = Arrays.toString(mmrh12(arr));
-                }
-            }
-        }
-
-    }
+}
